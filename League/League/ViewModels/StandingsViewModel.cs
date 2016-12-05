efd41cdb27ddd518fde6feaeb2955b1261f7782e
Austin <austin.laird07@gmail.com>
2016-12-05 02:08:24 -0500

    Initial version of the StandingsViewModel class

---
+++ League/League/ViewModels/StandingsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using League.Models;
+using System.Collections.Generic;
+
+namespace League.ViewModels
+{
+    public class StandingsViewModel
+    {
+        public StandingsViewModel()
+        {
+            var allTeams = App.TeamRepo.GetAllTeams();
+
+            Standings = allTeams;
+        }
+
+        public List<Team> Standings { get; set; }
+
+    }
+}
