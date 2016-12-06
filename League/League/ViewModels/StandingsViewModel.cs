00bf9eed361e3b11b3f7467a37f8e7f5743dd92a
Austin <austin.laird07@gmail.com>
2016-12-05 23:34:27 -0500

    Use TeamViewModel in StandingsViewModel

--- League/League/ViewModels/StandingsViewModel.cs
+++ League/League/ViewModels/StandingsViewModel.cs
@@ -1,18 +1,25 @@
 ﻿using League.Models;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace League.ViewModels
 {
     public class StandingsViewModel
     {
+        private List<Team> Teams;
+
+        public List<TeamViewModel> Standings {
+            get
+            {
+                return Teams.Select(t => new TeamViewModel(t)).ToList();
+            }
+        }
+
         public StandingsViewModel()
         {
             var allTeams = App.TeamRepo.GetAllTeams();
 
-            Standings = allTeams;
+            Teams = allTeams;
         }
-
-        public List<Team> Standings { get; set; }
-
     }
 }
