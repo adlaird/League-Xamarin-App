96ea49a4d26c3fe804d647100d397b5678e1c54c
Austin <austin.laird07@gmail.com>
2016-12-12 00:40:14 -0500

    Create extension method to convert from Team to TeamDTO

---
+++ League/League/Extensions/TeamExtensions.cs
@@ -0,0 +1,18 @@
+﻿using League.DTOs;
+using League.Models;
+
+namespace League.Extensions
+{
+    public static class TeamExtensions
+    {
+        public static TeamDTO ConvertToDTO(this Team team)
+        {
+            TeamDTO teamDTO = new TeamDTO();
+
+            teamDTO.Name = team.Name;
+            teamDTO.Record = team.Wins + "-" + team.Losses;
+
+            return teamDTO;
+        }
+    }
+}
