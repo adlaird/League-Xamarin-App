83107828bbfbde5620e7b0ae94c21ca1d617b8ed
Austin <austin.laird07@gmail.com>
2017-02-08 22:19:38 -0500

    Update TeamService unit tests

--- League.Tests/TeamServiceTests.cs
+++ League.Tests/TeamServiceTests.cs
@@ -16,7 +16,10 @@ namespace League.Tests
             var mockTeamRepo = new Mock<ITeamRepository>();
             mockTeamRepo.Setup(m => m.GetAllTeams()).Returns(new List<Team>());
 
-            var teamService = new TeamService(mockTeamRepo.Object);
+            var mockGameRepo = new Mock<IGameRepository>();
+            mockGameRepo.Setup(m => m.GetGamesByTeamId(It.IsAny<int>())).Returns(new List<Game>());
+
+            var teamService = new TeamService(mockTeamRepo.Object, mockGameRepo.Object);
 
             teamService.GetAllTeams();
 
@@ -24,11 +27,31 @@ namespace League.Tests
         }
 
         [Test]
+        public void GetAllTeams_returns_teams_with_calculated_records()
+        {
+            var mockTeamRepo = new Mock<ITeamRepository>();
+            mockTeamRepo.Setup(m => m.GetAllTeams()).Returns(new List<Team>() { new Team { Id = 0 } });
+
+            var mockGameRepo = new Mock<IGameRepository>();
+            mockGameRepo.Setup(m => m.GetGamesByTeamId(It.IsAny<int>())).Returns(new List<Game>() { new Game { HomeTeam = 0, AwayTeam = 1, HomeScore = 1, AwayScore = 0 } });
+
+            var teamService = new TeamService(mockTeamRepo.Object, mockGameRepo.Object);
+
+            var result = teamService.GetAllTeams();
+
+            mockTeamRepo.Verify(m => m.GetAllTeams(), Times.Once());
+            mockGameRepo.Verify(m => m.GetGamesByTeamId(0), Times.Once());
+
+            Assert.IsTrue(result[0].Record == "1-0");
+        }
+
+        [Test]
         public void Reset_calls_repo_Reset_function_once()
         {
             var mockTeamRepo = new Mock<ITeamRepository>();
+            var mockGameRepo = new Mock<IGameRepository>();
 
-            var teamService = new TeamService(mockTeamRepo.Object);
+            var teamService = new TeamService(mockTeamRepo.Object, mockGameRepo.Object);
 
             teamService.Reset();
 
